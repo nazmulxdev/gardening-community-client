986238f909063d133a72de41cd0de99bbe1e5bc6
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-05-21 11:37:15 +0600

    configured private route

--- src/Private/PrivateRoute.jsx
+++ src/Private/PrivateRoute.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from "../Context/AuthContext";
+import { Navigate, useLocation } from "react-router";
+import LoadingSpinner from "../Utilities/LoadingSpinner";
 
-const PrivateRoute = () => {
-  return (
-    <div>
-      <p>this is private route</p>
-    </div>
-  );
+const PrivateRoute = ({ children }) => {
+  const { presentUser, loading } = useContext(AuthContext);
+  const location = useLocation();
+  if (loading) {
+    return <LoadingSpinner></LoadingSpinner>;
+  }
+  if (presentUser) {
+    return children;
+  }
+  return <Navigate state={location.pathname} to="/login"></Navigate>;
 };
 
 export default PrivateRoute;
